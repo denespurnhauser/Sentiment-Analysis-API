b9a3c6dabcaa31134a67573509689fb10ab56df2
denespurnhauser <143980282+denespurnhauser@users.noreply.github.com>
2023-09-22 08:08:47 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
+from langchain.prompts import PromptTemplate\nfrom langchain.llms import OpenAI\nfrom langchain.chains import LLMChain
 
+prompt_template = "What is a good name for a company that makes {product}?"
+
+llm = OpenAI(temperature=0)
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=PromptTemplate.from_template(prompt_template)
+)
+llm_chain("colorful socks")
